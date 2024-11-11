8324ee290d8cbfc2826b9ac01184b18ce0a0cbbd
Emily C. Zabor <emily.zabor@gmail.com>
2024-11-11 10:13:26 -0500

    change to missingness generation

--- data-raw/bc_rt_data.R
+++ data-raw/bc_rt_data.R
@@ -1,86 +1,163 @@
 ## code to prepare `bc_rt_data` dataset goes here
 
-library(msm) # need for truncated normal distribution
+# load needed libraries --------------------------------------------------------
+# first run install.packages(".") to install package "." from CRAN if needed
+library(mvtnorm)
 library(tibble)
 library(dplyr)
 
-set.seed(20240305)
 
-n <- 3000 # sample size
+# Set all of the fixed parameters ----------------------------------------------
 
-# generate covariates
-rt <- rbinom(n, 1, p = .6)
-age_dx_yrs <- rtnorm(n, mean = 60 - (rt * 5), sd = 14, lower = 18, upper = 95)
-tumor_size_cm <- rtnorm(n, mean = 2.1 + (rt * .4), sd = 1, lower = 0)
-n_ln_pos_3_vs_1or2 <- rbinom(n, 1, p = .07 + (rt * .13))
-nodal_ratio <- rtnorm(n, mean = .2 + (rt * .06), sd = .22, lower = .02,
-                      upper = 1)
-lvi <- rbinom(n, 1, p = .35 + (rt * .1))
-er_or_pr_pos <- rbinom(n, 1, p = .88 - (rt * .01))
-her2_pos <- rbinom(n, 1, p = .16 + (rt * .02))
-quadrant_inner_vs_other <- rbinom(n, 1, .16)
-optimal_systemic_therapy <- rbinom(n, 1, p = .84 + (rt * .09))
+# sample size
+n <- 3500
 
-# make grade a categorical variable
-# But also with indicators for the model
-grade <- runif(n)
-grade_3cat <- case_when(
-  rt == 0 & grade < .14 ~ "I",
-  rt == 0 & grade >= .14 & grade < .62 ~ "II",
-  rt == 0 & grade >= .62 ~ "III",
-  rt == 1 & grade < .10 ~ "I",
-  rt == 1 & grade >= .10 & grade < .54 ~ "II",
-  rt == 1 & grade >= .54 ~ "III"
-)
-grade_ind3 = ifelse(grade_3cat == "III", 1, 0)
-grade_ind2 = ifelse(grade_3cat == "II", 1, 0)
+# Weibull shape and scale parameters
+event_shape <- 1.6
+event_scale <- 122
+cens_shape <- 2.6
+cens_scale <- 8.2
+
+# log(HR) for each covariate
+beta_x1 <- log(0.8)
+beta_x2 <- log(1.05)
+beta_x3 <- log(1.25)
+beta_x4 <- log(1.54)
+beta_x5 <- log(1.18)
+beta_x6 <- log(1.45)
+beta_x7 <- log(1.10)
+beta_x8 <- log(0.76)
+beta_x9 <- log(0.64)
+beta_x10 <- log(1.25)
+beta_x11 <- log(0.48)
+
+# proportion of missingness for each covariate
+p_m_x1 <- 0.05
+p_m_x3 <- 0.15
+p_m_x4 <- 0.30
+
+# log(OR) for joint missingness
+gamma_11 <- 0
+gamma_13 <- log(4.21)
+gamma_14 <- log(2.78)
+
+# log(OR) for covariates related to missingness
+gamma_21 <- log(1.05)
+gamma_23 <- log(0.8)
+gamma_24 <- log(0.7)
+
+# Covariate mean vector
+mymu <- c(0.6145, 57.6495, 2.3665, 0.4225, 0.1538, 0.2421, 0.4142, 0.8680,
+          0.1695, 0.1636, 0.8891)
+
+# Covariate covariance matrix
+mysigma <- matrix(
+  c(0.2370, -1.3349, 0.0812, 0.0247, 0.0298, 0.0134, 0.0280, -0.0069,
+    0.0039, 0.0002, 0.0223, -1.3349, 196.2990, 0.6471, -0.6314, -0.0759,
+    0.3167, -0.7944, 0.0156, -0.1261, 0.0294, -0.8057, 0.0812, 0.6471,
+    1.0967, 0.0680, 0.0452, 0.0063, 0.0740, -0.0190, -0.0080, -0.0147,
+    -0.0075, 0.0247, -0.6314, 0.0680, 0.2441, 0.0063, -0.0089, 0.0468,
+    -0.0581, 0.0484, 0.0003, -0.0139, 0.0298, -0.0759, 0.0452, 0.0063,
+    0.1302, 0.0070, 0.0214, -0.0026, 0.0016, -0.0001, 0.0043, 0.0134,
+    0.3167, 0.0063, -0.0089, 0.0070, 0.0558, -0.0015, 0.0050, -0.0026,
+    0.0003, 0.0000, 0.0280, -0.7944, 0.0740, 0.0468, 0.0214, -0.0015,
+    0.2427, -0.0119, 0.0131, 0.0017, 0.0012, -0.0069, 0.0156, -0.0190,
+    -0.0581, -0.0026, 0.0050, -0.0119, 0.1146, -0.0318, 0.0001, 0.0086,
+    0.0039, -0.1261, -0.0080, 0.0484, 0.0016, -0.0026, 0.0131, -0.0318,
+    0.1408, 0.0050, -0.0267, 0.0002, 0.0294, -0.0147, 0.0003, -0.0001,
+    0.0003, 0.0017, 0.0001, 0.0050, 0.1369, 0.0014, 0.0223, -0.8057,
+    -0.0075, -0.0139, 0.0043, 0.0000, 0.0012, 0.0086,  -0.0267, 0.0014, 0.0986),
+  nrow = 11, ncol = 11, byrow = TRUE)
 
 
-# weibull event and censoring distribution parameters
-event_shape <- 1.54
-event_scale <- 21.55
-cens_shape <- 2.55
-cens_scale <- 8.22
+# Generate complete data ------------------------------------------------------
 
-# setup the formula
-xform <- rt * log(.8) + age_dx_yrs * log(1.05) + tumor_size_cm * log(1.24) +
-  grade_ind3 * log(1.66) + grade_ind2 * log(1.26) +
-  n_ln_pos_3_vs_1or2 * log(1.08) +
-  nodal_ratio * log(1.23) + lvi * log(1.05) + er_or_pr_pos * log(.86) +
-  her2_pos * log(.64) + quadrant_inner_vs_other * log(1.16) +
-  optimal_systemic_therapy * log(.48)
+# set seed for replication
+set.seed(20241111)
 
-# generate the event and censoring times
-x <- rweibull(n, event_shape, event_scale / exp(xform / event_shape))
-c <- rweibull(n, cens_shape, cens_scale)
+# Generate covariates
+covs <-
+  rmvnorm(n = n, mean = mymu, sigma = mysigma) |>
+  as_tibble(.name_repair = ~ paste0("x", 1:11)) |>
+  mutate(
+    x1 = ifelse(x1 <= 0.5, 0, 1),
+    x4 = ifelse(x4 <= 0.5, 0, 1),
+    x5 = ifelse(x5 <= 0.5, 0, 1),
+    x7 = ifelse(x7 <= 0.5, 0, 1),
+    x8 = ifelse(x8 <= 0.5, 0, 1),
+    x9 = ifelse(x9 <= 0.5, 0, 1),
+    x10 = ifelse(x10 <= 0.5, 0, 1),
+    x11 = ifelse(x11 <= 0.5, 0, 1))
 
-# create observed time and event indicator
-t <- pmin(x, c)
-delta <- ifelse(x <= c, 1, 0)
+# Generate event and censoring times
+u <- runif(n)
+t <- event_scale *
+  ((-log(u)) /
+     exp(beta_x1 * covs$x1 +
+           beta_x2 * covs$x2 +
+           beta_x3 * covs$x3 +
+           beta_x4 * covs$x4 +
+           beta_x5 * covs$x5 +
+           beta_x6 * covs$x6 +
+           beta_x7 * covs$x7 +
+           beta_x8 * covs$x8 +
+           beta_x9 * covs$x9 +
+           beta_x10 * covs$x10 +
+           beta_x11 * covs$x11))^(1/event_shape)
 
-# Make some covariate data missing completely at random
-rand1 <- rbinom(n, 1, p = 0.05)
-tumor_size_cm <- ifelse(rand1 == 1, NA, tumor_size_cm)
-rand2 <- rbinom(n, 1, p = 0.03)
-er_or_pr_pos <- ifelse(rand2 == 1, NA, er_or_pr_pos)
-rand3 <- rbinom(n, 1, p = 0.04)
-her2_pos <- ifelse(rand3 == 1, NA, her2_pos)
+c <- rweibull(n, cens_shape, cens_scale) # weibull censoring times
+
+# Combine into single time/event indicator
+s <- pmin(t, c)
+delta <- ifelse(t <= c, 1, 0)
+
+# Induce covariate missingness -------------------------------------------------
+
+# Generate missingness
+gamma_01 <- log(p_m_x1 / (1 - p_m_x1)) - gamma_21 * mean(covs$x2)
+xg1 <- gamma_01 + gamma_21 * covs$x2
+mp1 <- exp(xg1) / (1 + exp(xg1))
+m1 <- rbinom(n, 1, prob = mp1)
+x1_miss = ifelse(m1 == 1, NA, covs$x1)
+
+gamma_03 <- log(p_m_x3 / (1 - p_m_x3)) - gamma_13 * p_m_x1 -
+  gamma_23 * mean(covs$x5)
+xg3 <- gamma_03 + gamma_13 * ifelse(is.na(x1_miss), 1, 0) + gamma_23 * covs$x5
+mp3 <- exp(xg3) / (1 + exp(xg3))
+m3 <- rbinom(n, 1, prob = mp3)
+x3_miss = ifelse(m3 == 1, NA, covs$x3)
+
+gamma_04 <- log(p_m_x4 / (1 - p_m_x4)) - gamma_14 * p_m_x3 -
+  gamma_24 * mean(covs$x6)
+xg4 <- gamma_04 + gamma_14 * ifelse(is.na(x3_miss), 1, 0) + gamma_24 * covs$x6
+mp4 <- exp(xg4) / (1 + exp(xg4))
+m4 <- rbinom(n, 1, prob = mp4)
+x4_miss = ifelse(m4 == 1, NA, covs$x4)
+
 
 # put everything into dataset
 bc_rt_data <- tibble(
-  time = t,
-  event = delta,
-  rt = rt,
-  age_dx_yrs = age_dx_yrs,
-  tumor_size_cm = tumor_size_cm,
-  grade = grade_3cat,
-  n_ln_pos_3_vs_1or2 = n_ln_pos_3_vs_1or2,
-  nodal_ratio = nodal_ratio,
-  lvi = lvi,
-  er_or_pr_pos = er_or_pr_pos,
-  her2_pos = her2_pos,
-  quadrant_inner_vs_other = quadrant_inner_vs_other,
-  optimal_systemic_therapy = optimal_systemic_therapy
+  os_years = s,
+  os_event = delta,
+  rt = x1_miss,
+  tumor_size_cm = x3_miss,
+  grade = x4_miss
+) |>
+  bind_cols(covs) |>
+  # remove the complete data
+  select(!c(x1, x3, x4)) |>
+  rename(age_dx_yrs = x2,
+         n_ln_pos_3_vs_1or2 = x5,
+         nodal_ratio = x6,
+         lvi = x7,
+         er_or_pr_pos = x8,
+         her2_pos = x9,
+         quadrant_inner_vs_other = x10,
+         optimal_systemic_therapy = x11) |>
+  # generate a participant ID
+  mutate(id = row_number())|>
+  select(id, os_years, os_event, rt, age_dx_yrs, everything())
+
 )
 
 # create the dataset
